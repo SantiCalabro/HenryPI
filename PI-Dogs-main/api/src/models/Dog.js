ded46dee4802dd188353fb36ad3c7f28d47741d8
SantiCalabro <santi.calabro93@gmail.com>
2022-10-23 18:40:48 -0300

    models

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -1,12 +1,44 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = sequelize => {
   // defino el modelo
-  sequelize.define('dog', {
+  const hash = value => {
+    return value + "db";
+  };
+
+  sequelize.define("Raza", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      set(value) {
+        this.setDataValue("id", hash(value));
+      },
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    height: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    yearsOfLife: {
+      type: DataTypes.INTEGER(2),
+    },
+  });
+
+  sequelize.define("Temperamento", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
     },
   });
 };
